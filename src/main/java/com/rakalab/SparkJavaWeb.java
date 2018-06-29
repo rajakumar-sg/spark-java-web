2b0aeb17d95ea4eb85fdb8d6d804f629b5457b4c
raj <torajakumar@gmail.com>
2018-06-29 20:45:19 +0800

    adding logging

--- src/main/java/com/rakalab/SparkJavaWeb.java
+++ src/main/java/com/rakalab/SparkJavaWeb.java
@@ -7,14 +7,19 @@ import java.util.stream.Collectors;
 
 public class SparkJavaWeb {
     public static void main(String[] args) {
+        setupLogger();
         Spark.get("/hello",
                 (req, res) -> String.format("Hello, %s",
                         req.queryMap().toMap()
                         .entrySet()
                         .stream()
-                        .map(e -> e.getKey() + Arrays.toString(e.getValue()))
+                        .map(e -> e.getKey() + "=" + Arrays.toString(e.getValue()))
                         .collect(Collectors.toList())
                 ));
+
+    }
+
+    private static void setupLogger() {
 
     }
 }
